e14db95bf4fc774525bcaf85344821ba8fc91962
wiem2000 <wiembenmlouka@gmail.com>
2024-11-08 20:21:19 +0100

    new commit

--- src/main/java/com/example/dockerspringdemo/controller/HelloController.java
+++ src/main/java/com/example/dockerspringdemo/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 
 import org.springframework.web.bind.annotation.PathVariable;
 
