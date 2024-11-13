5f5c5c63f938812673008ea4f24aaa2193b3857b
wiem2000 <wiembenmlouka@gmail.com>
2024-11-14 00:57:27 +0100

    new commit

--- src/main/java/com/example/dockerspringdemo/entity/UsersEntity.java
+++ src/main/java/com/example/dockerspringdemo/entity/UsersEntity.java
@@ -5,14 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.Setter;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@Data @AllArgsConstructor @NoArgsConstructor @ToString
+@Getter
+@Setter
 public class UsersEntity {
 	
 	@Id
